2a2833ba930e985b0134e25c2ee62edc35b9536f
Zadith Manotas Pretel <zadith@gmail.com>
2017-07-01 15:43:54 -0500

    Agregado index.html en carpeta por defecto para archivos HTML de thymeleaf y creación de clase MainController para despliegue de vista

---
+++ src/main/java/handzath/springboot/thymeleaf3/web/controller/MainController.java
@@ -0,0 +1,19 @@
+package handzath.springboot.thymeleaf3.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value = { "", "/" }, method = RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("message", "Mensaje devuelto desde un @Controller de Spring");
+		mav.setViewName("index");
+		return mav;
+	}
+
+}
