0462b35a1c45f6e7871ed35db1f879a2cf27f86c
Zadith Manotas Pretel <zadith@gmail.com>
2017-07-01 15:24:12 -0500

    Creación de estructura maven y clase inicial Application

---
+++ src/main/java/handzath/springboot/thymeleaf3/Application.java
@@ -0,0 +1,13 @@
+package handzath.springboot.thymeleaf3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
